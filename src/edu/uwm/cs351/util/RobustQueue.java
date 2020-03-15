7d0d0f665e37ec5520eb4f01e707a76a73a1a215
Anton <Anton@DESKTOP-JOK4DJE.lan1>
2020-03-15 12:51:35 -0500

    I got the first 8 test working now I need to work on the iterator to unlock the  next test. I also answered all the UnlockTest questions

--- src/edu/uwm/cs351/util/RobustQueue.java
+++ src/edu/uwm/cs351/util/RobustQueue.java
@@ -1,25 +1,120 @@
 // Anton Langbruttig
 package edu.uwm.cs351.util;
- 
+import java.util.AbstractQueue;
+import java.util.Iterator;
+import java.util.Queue;
+
+
+
 /******************************************************************************
  * This is generic queue implemented with a doubly-linked list and a dummy node
  ******************************************************************************/
 
-public class RobustQueue <E> {
+@SuppressWarnings("rawtypes")
+public class RobustQueue <E> extends AbstractQueue {
 
-	private Node <E> head;
+	private Node <E> dummy;
 	private int manyNodes = 0;
-	
-	
-	
+
 	static class Node <T> {
-		private T data;
-		private Node<T> next;
-		private Node<T> prev;
+		T data;
+		Node<T> next, prev;
 		Node(T t, Node<T> n, Node <T> p){
 			data = t;
 			next = n;
 			prev = p;
+		}
+	}
+
+	private boolean wellFormed () {
+		if(manyNodes < 0 )return false;
+		if(dummy == null) return false;
+		return true;
+
+	}
+
+	/**
+	 * Instantiates a new RobustQueue.
+	 *
+	 * @postcondition
+	 * 	This RobustQueue is empty except for the dummy node
+	 */
+	public RobustQueue() {
+		dummy = new Node <E>(null,null,null);
+		dummy.next = dummy;
+		dummy.prev = dummy;
+
+		assert wellFormed();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean offer(Object e) {
+		assert wellFormed();
+
+		Node<E> n = new Node (e,null,null);
+		n.next = dummy;
+		dummy.prev.next = n;
+		n.prev = dummy.prev;
+		dummy.prev = n;
+
+		++manyNodes;
+		assert wellFormed();
+
+		return true;
+	}
+
+	@Override
+	public Object poll() {
+		// TODO Auto-generated method stub
+		assert wellFormed();
+
+
+
+		Node s = dummy.next;
+
+		dummy.next.prev = dummy;
+		dummy.next = dummy.next.next;
+		if(manyNodes != 0) --manyNodes;
+		assert wellFormed();
+
+		return s.data;
+	}
+
+	@Override
+	public Object peek() {
+		// TODO Auto-generated method stub
+
+		return dummy.next.data;
+	}
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		assert wellFormed();
+		return new MyIterator();
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return manyNodes;
+	}
+
+	private class MyIterator implements Iterator<E> {
+
+		Node cursor = dummy;
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public E next() {
+			// TODO Auto-generated method stub
+			return null;
 		}
 	}
 }
